c35e45c6d34820a7aa504f6c860b4fa8d5a14488
joao <joao_victor.dp@hotmail.com>
2018-04-30 23:01:53 -0300

    Adicionado validação de CPF e CNPJ

---
+++ Validação CPF-CNPJ.js
@@ -0,0 +1,69 @@
+/*
+ Validador de CNPJ, valida se for um CNPJ e Retorna TRUE ou FALSE
+*/
+function valida_cpf( valor ) {
+
+    // Garante que o valor é uma string
+    valor = valor.toString();
+    
+    // Remove caracteres inválidos do valor
+    valor = valor.replace(/[^0-9]/g, '');
+
+
+    // Captura os 9 primeiros dígitos do CPF
+    // Ex.: 02546288423 = 025462884
+    var digitos = valor.substr(0, 9);
+
+    // Faz o cálculo dos 9 primeiros dígitos do CPF para obter o primeiro dígito
+    var novo_cpf = calc_digitos_posicoes( digitos );
+
+    // Faz o cálculo dos 10 dígitos do CPF para obter o último dígito
+    var novo_cpf = calc_digitos_posicoes( novo_cpf, 11 );
+
+    // Verifica se o novo CPF gerado é idêntico ao CPF enviado
+    if ( novo_cpf === valor ) {
+        // CPF válido
+        return true;
+    } else {
+        // CPF inválido
+        return false;
+    }
+    
+} 
+
+/*
+ Validador de CNPJ, valida se for um CNPJ e Retorna TRUE ou FALSE
+*/
+function valida_cnpj ( valor ) {
+
+    // Garante que o valor é uma string
+    valor = valor.toString();
+    
+    // Remove caracteres inválidos do valor
+    valor = valor.replace(/[^0-9]/g, '');
+
+    
+    // O valor original
+    var cnpj_original = valor;
+
+    // Captura os primeiros 12 números do CNPJ
+    var primeiros_numeros_cnpj = valor.substr( 0, 12 );
+
+    // Faz o primeiro cálculo
+    var primeiro_calculo = calc_digitos_posicoes( primeiros_numeros_cnpj, 5 );
+
+    // O segundo cálculo é a mesma coisa do primeiro, porém, começa na posição 6
+    var segundo_calculo = calc_digitos_posicoes( primeiro_calculo, 6 );
+
+    // Concatena o segundo dígito ao CNPJ
+    var cnpj = segundo_calculo;
+
+    // Verifica se o CNPJ gerado é idêntico ao enviado
+    if ( cnpj === cnpj_original ) {
+        return true;
+    }
+    
+    // Retorna falso por padrão
+    return false;
+    
+}
